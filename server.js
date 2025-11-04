3386d4770cbae7f82d86a0a4ae6ad52eb725f1dd
rudrateja01 <rudrateja8897@gmail.com>
2025-11-04 20:53:34 +0530

    Ready for Vercel deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send("📚 Welcome to Book Catalog API!");
+  res.send("Welcome to Book Catalog API!");
 });
 
 app.use("/api/users",userRoutes);
